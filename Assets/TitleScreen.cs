5e51b4ceb99aa70335ad6de2b12ebe0f3fc79b73
jonobarel <jonobarel@users.noreply.github.com>
2022-05-03 01:21:37 +0300

    title screen with A prompt
    Gamepad "A" is registered to the Debug console.

---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Player1_Jump") || 
+            Input.GetButtonDown("Player2_Jump") ||
+            Input.GetButtonDown("Player3_Jump") ||
+            Input.GetButtonDown("Player4_Jump")
+            )
+        { Debug.Log("Joystick Button A pressed");
+        }
+    }
+}
