fdd89feabbea14a19183324ef9312209b4c35356
jonobarel <jonobarel@users.noreply.github.com>
2022-11-08 11:32:14 +0200

    setup on new system

--- Assets/Scripts/Internal/Bindings/CarriageSceneInstaller.cs
+++ Assets/Scripts/Internal/Bindings/CarriageSceneInstaller.cs
@@ -5,10 +5,14 @@ namespace ZeroPrep.MineBuddies
 {
     public class CarriageSceneInstaller : MonoInstaller
     {
-        
+        public override void InstallSettings()
+        {
+            
+        }
         public override void InstallBindings()
         {
             Container.Bind<Engine>().FromNew().AsSingle().WithArguments(100f, 90f, 2f);
+            Container.Bind<HazardManagerGO>().FromInstance(FindObjectOfType<HazardManagerGO>());
         }
     }
 }
