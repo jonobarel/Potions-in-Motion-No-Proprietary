4f481f4931318f1a949f803648795bc2b82d063f
jonobarel <jonobarel@users.noreply.github.com>
2022-12-29 15:33:00 +0200

    replace skins with sprites

--- Assets/Scripts/Internal/GameSystems/Characters/PlayerSkinsListPrefab.cs
+++ Assets/Scripts/Internal/GameSystems/Characters/PlayerSkinsListPrefab.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace ZeroPrep.MineBuddies
 {
     public class PlayerSkinsListPrefab : MonoBehaviour
     {
-        [SerializeField] private GameObject[] playerSkins;
+        [SerializeField] private Sprite[] playerSkins;
+
+        public List<Sprite> PlayerSkins => playerSkins.ToList();
     }
 }
